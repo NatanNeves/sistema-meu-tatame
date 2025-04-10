473a42aac9a6b41d9c4d42e9223558db13121ca0
NatanNeves <dev.natanbarbosa@gmail.com>
2025-04-10 20:19:41 -0300

    feat: adicionando função de criar aluno e excluir aluno

--- src/main/java/com/web/meutatame/tcc/controllers/AlunosController.java
+++ src/main/java/com/web/meutatame/tcc/controllers/AlunosController.java
@@ -5,7 +5,7 @@ import com.web.meutatame.tcc.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +18,24 @@ public class AlunosController {
     @GetMapping("/alunos")
     public String index(Model model){
         List<Aluno> alunos = alunoRepository.findAll();
-        System.out.println("Alunos encontrados: " + alunos.size());
         model.addAttribute("alunos", alunos);
         return "alunos/index";
     }
 
+    @GetMapping("/alunos/novo")
+    public String novo(){
+        return "alunos/novo";
+    }
+
+    @PostMapping("/alunos/criar")
+    public String criar(Aluno aluno){
+         alunoRepository.save(aluno);
+         return "redirect:/alunos";
+    }
+
+    @GetMapping("/alunos/{id}/excluir")
+    public String excluir(@PathVariable int id) {
+        alunoRepository.deleteById(id); // Deletar um aluno pelo ID
+        return "redirect:/alunos"; // Redireciona para a lista de alunos
+    }
 }
